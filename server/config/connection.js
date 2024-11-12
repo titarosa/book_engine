862f8be0437759de82dad7973f52bc5032d1fc85
Talita Rosa de Oliveira <talitarosa.02@hotmail.com>
2024-11-12 16:42:31 -0500

    Fixing mongo URI

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,12 @@
+require('dotenv').config();  // Ensure env variables are loaded
+
 const mongoose = require('mongoose');
 
 // Use the MONGODB_URI from the .env file if it's defined, else fallback to local connection (for development)
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
 
 mongoose.connection.on('connected', () => {
   console.log('Connected to MongoDB');
