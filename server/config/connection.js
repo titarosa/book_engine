6fe7b5687ec5bb36bce7a1e7bca0e93049487fb1
Talita Rosa de Oliveira <talitarosa.02@hotmail.com>
2024-11-12 16:20:42 -0500

    Update MongoDB connection to use Atlas URI

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,9 +1,14 @@
 const mongoose = require('mongoose');
 
 // Use the MONGODB_URI from the .env file if it's defined, else fallback to local connection (for development)
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks');
+
+mongoose.connection.on('connected', () => {
+  console.log('Connected to MongoDB');
+});
+
+mongoose.connection.on('error', (err) => {
+  console.log('MongoDB connection error: ', err);
 });
 
 module.exports = mongoose.connection;
